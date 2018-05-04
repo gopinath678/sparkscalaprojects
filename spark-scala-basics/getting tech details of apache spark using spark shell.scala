f278636063a11efbbb771909d106970258aad537
Jose Praveen Thiyagarajan <tjosepraveen@gmail.com>
2018-05-04 12:58:58 +0530

    'sparkscalalearning'

---
+++ spark-scala-basics/getting tech details of apache spark using spark shell.scala
@@ -0,0 +1,22 @@
+/**
+ * Getting Technical with Spark
+ */
+
+// *****************************************************************************************************
+// *** SparkContext and SparkSession
+//SparkContext
+sc -- prints the sparkcontext object.
+sc.version -- prints the spark context version
+sc.getClass -- prints the sparkcontext class
+sc.appName -- prints the app name
+sc.getClass
+sc.getClass.getName -- prints the name of the sparkcontext class
+sc //hit tab, autocomplete will kick in
+sc.uiWebUrl -- prints the web ui url
+sc.applicationId -- prints the application id
+sc.sparkUser -- prints the spark username
+
+//SparkSession
+spark -- prints the sparksession object
+spark.getClass.getName -- prints the sparksession class
+spark.sparkContext -- prints the sparkcontext object.
